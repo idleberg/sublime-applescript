c393b310950135d1f750c33628fb9ae969e74631
idleberg <jan@idleberg.com>
2016-04-29 11:44:20 +0200

    bump linter

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,14 @@
  /*
- * sublimetext-gruntfile.js
- * https://github.com/idleberg/sublimetext-gruntfile.js
- *
- * Copyright (c) 2014 Jan T. Sott
- * Licensed under the MIT license.
- */
+  * sublimetext-gruntfile.js
+  * https://github.com/idleberg/sublimetext-gruntfile.js
+  *
+  * Copyright (c) 2014-2016 Jan T. Sott
+  * Licensed under the MIT license.
+  */
  
  module.exports = function(grunt) {
 
- 	var jsonFiles = [
+    var jsonFiles = [
         '**/*.JSON-tmLanguage',
         '**/*.sublime-build',
         '**/*.sublime-commands',
@@ -18,52 +18,95 @@
         '**/*.sublime-menu',
         '**/*.sublime-settings',
         '**/*.sublime-theme',
-        'messages.json'
+        'messages.json',
+        '!node_modules/**/*.*'
+    ];
+
+    var pyFiles = [
+        '*.py'
+    ];
+
+    var ymlFiles = [
+        '**/*.sublime-syntax'
     ];
 
     var xmlFiles = [
-    	'**/*.plist',
-    	'**/*.sublime-snippet',
-    	'**/*.tmCommand',
-    	'**/*.tmLanguage',
-    	'**/*.tmPreferences',
-    	'**/*.tmSnippet'
+        '**/*.plist',
+        '**/*.sublime-snippet',
+        '**/*.tmCommand',
+        '**/*.tmLanguage',
+        '**/*.tmPreferences',
+        '**/*.tmSnippet',
+        '**/*.tmTheme',
+        '**/*.xml',
+        '*.bbcolors',
+        '*.dvtcolortheme',
+        '*.icls',
+        '*.itermcolors',
+        '*.terminal',
+        '!node_modules/**/*.*'
     ];
 
- 	grunt.initConfig({
+    grunt.initConfig({
 
- 		// default task
-		jsonlint: {
-		  files: {
-		    src: jsonFiles
-		  }
-		},
+        // default task
+        jsonlint: {
+          files: {
+            src: jsonFiles
+          }
+        },
 
- 		xml_validator: {
- 			files: {
- 				src: xmlFiles
- 			},
- 		},
+        yamllint: {
+            files: {
+                src: ymlFiles
+            },
+        },
 
-		// watch task
+        xml_validator: {
+            files: {
+                src: xmlFiles
+            },
+        },
+
+        // watch task
         watch: {
-		    json: {
-		        files: jsonFiles,
-		        tasks: ['jsonlint']
-		    },
-		    xml: {
-		        files: xmlFiles,
-		        tasks: ['xml_validator']
-		    }
-		}
- 	});
+            json: {
+                files: jsonFiles,
+                tasks: ['jsonlint']
+            },
+            yml: {
+                files: ymlFiles,
+                tasks: ['yamllint']
+            },
+            xml: {
+                files: xmlFiles,
+                tasks: ['xml_validator']
+            }
+        },
 
- 	grunt.loadNpmTasks('grunt-contrib-watch');
- 	grunt.loadNpmTasks('grunt-xml-validator');
- 	grunt.loadNpmTasks('grunt-jsonlint');
- 	grunt.registerTask('default', ['jsonlint', 'xml_validator']);
+        // pylint task, not yet part of default
+        pylint: {
+          files: {
+            options: {
+                errorsOnly: true
+            },
+            src: pyFiles
+          }
+        },
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-jsonlint');
+    grunt.loadNpmTasks('grunt-pylint');
+    grunt.loadNpmTasks('grunt-yamllint');
+    grunt.loadNpmTasks('grunt-xml-validator');
+    grunt.registerTask('default', 'lint');
 
     // task shortcuts
- 	grunt.registerTask('json', 'jsonlint');
- 	grunt.registerTask('xml', 'xml_validator');
+    grunt.registerTask('json',   'jsonlint');
+    grunt.registerTask('lint',  ['jsonlint', 'yamllint', 'xml_validator']);
+    grunt.registerTask('py',     'pylint');
+    grunt.registerTask('travis', 'lint');
+    grunt.registerTask('xml',    'xml_validator');
  };
